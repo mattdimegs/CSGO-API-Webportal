2c3c2215afab4aa10f54d063f4c8a3d222fb2900
mattdimegs <mdimeglio7@optonline.net>
2023-04-23 17:23:46 -0400

    Add's Search Button to Home Page

--- src/localComponents/home/index.tsx
+++ src/localComponents/home/index.tsx
@@ -1,22 +1,43 @@
+import React, { useState } from "react";
 import { TextField } from "@mui/material";
+import { LoadingButton } from "@mui/lab";
+import SearchIcon from "@mui/icons-material/Search";
 import styles from "./index.module.css";
 
 const Home = () => {
+  const [loading, setLoading] = useState(false);
+  function handleClick() {
+    setLoading(true);
+  }
+
   return (
-    <div className={`${styles.searchFieldHome}`}>
-      <TextField
-        InputProps={{
-          sx: {
-            "& input": {
-              textAlign: "center",
+    <>
+      <div className={`${styles.searchFieldHome}`}>
+        <TextField
+          InputProps={{
+            sx: {
+              "& input": {
+                textAlign: "center",
+              },
             },
-          },
-        }}
-        id="outlined-search"
-        label="Username"
-        type="search"
-      />
-    </div>
+          }}
+          id="outlined-search"
+          label="Steam ID"
+          type="search"
+        />
+      </div>
+      <div className={`${styles.searchButtonHome}`}>
+        <LoadingButton
+          onClick={handleClick}
+          startIcon={<SearchIcon />}
+          loading={loading}
+          loadingPosition="start"
+          variant="outlined"
+        >
+          <span>Search</span>
+        </LoadingButton>
+      </div>
+    </>
   );
 };
 
